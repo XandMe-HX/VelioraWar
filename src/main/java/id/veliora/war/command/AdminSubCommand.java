6acfffcaa2c77cf3ece79d20799e084af6c45e37
XandMe Studio <kevindwinurhuda@gmail.com>
2026-08-15 20:30:12 +0700

    Document warp and green team commands

--- src/main/java/id/veliora/war/command/AdminSubCommand.java
+++ src/main/java/id/veliora/war/command/AdminSubCommand.java
@@ -6,11 +6,12 @@ import org.bukkit.command.CommandSender;
 public final class AdminSubCommand {
     public void sendHelp(CommandSender sender) {
         sender.sendMessage(TextUtil.component("&8&m---------------- &b&lVelioraWar Admin &8&m----------------"));
+        sender.sendMessage(TextUtil.component("&f/vgwar setwarp &7- set lokasi kembali setelah war"));
         sender.sendMessage(TextUtil.component("&f/vgwar pos1 &7- titik pertama region"));
         sender.sendMessage(TextUtil.component("&f/vgwar pos2 &7- titik kedua region"));
         sender.sendMessage(TextUtil.component("&f/vgwar claim &7- tampilkan ID: 1 Sword, 2 Mace, 3 Crystal, 4 All"));
         sender.sendMessage(TextUtil.component("&f/vgwar claim <1-4> &7- simpan region sesuai arena"));
-        sender.sendMessage(TextUtil.component("&f/vgwar spawn <merah|biru> &7- berdiri di dalam claim arena"));
+        sender.sendMessage(TextUtil.component("&f/vgwar spawn <merah|hijau> &7- berdiri di dalam claim arena"));
         sender.sendMessage(TextUtil.component("&f/vgwar flag &7- berdiri di dalam claim lalu buka aturan arena"));
         sender.sendMessage(TextUtil.component("&f/vgwar enable|disable <arena>"));
         sender.sendMessage(TextUtil.component("&f/vgwar setnpc <1|2> &7- set NPC refill (maks. 2)"));
